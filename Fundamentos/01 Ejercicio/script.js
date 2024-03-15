f3ab0af45dc6c335b04f8caae43d3bdbefd81273
kamasutras <ignaciolizana@liceovvh.cl>
2024-03-15 12:20:50 -0300

    ok

--- Fundamentos/01 Ejercicio/script.js
+++ Fundamentos/01 Ejercicio/script.js
@@ -16,10 +16,13 @@ let varNula = null;
 // varialble con Const 
 
 const frutas =  [[1, "melones"],[2, "peras"],[3, "sandias"],[4, "manzanas"]];
-/* const biografia = {
-    nombre = "ignacio", nacionalidad = "chilena", profesion = "Estudiante del VVH"
-    console.log("Mi nombre es " + nombre + " soy de nacionalidad " + nacionalidad  + " y soy " +  profesion)
-}; */
+
+
+const objeto = {
+    nombre : "ignacio", 
+    nacionalidad : "chilena", 
+    profesion : "Estudiante del VVH"
+}; 
 
 function suma(num1, num2){
     let resultado;
@@ -29,8 +32,11 @@ function suma(num1, num2){
 //console.log("la suma de 10+5 es = "+ suma(10, 5));
 
 
-console.log("Variable const constante:\n"+constante+"Variable var global:\n"+global+"\nVariable (let) local:\n"+local+"\nVariable numero Decimal:\n"+
-myDecimal+"\nVariable numero entero:\n"+numEntero+"\nVariable String (Cadena):\n"
-+string+"\nBoolean true (verdadero):\n"+booleanTrue+"\nBoolean false (falso):\n"+
+console.log("Variable const constante:\n"+constante+"Variable var global:\n"+
+global+"\nVariable (let) local:\n"+local+"\nVariable numero Decimal:\n"+
+myDecimal+"\nVariable numero entero:\n"+numEntero+"\nVariable String (Cadena):\n"+
+string+"\nBoolean true (verdadero):\n"+booleanTrue+"\nBoolean false (falso):\n"+
 booleanFalse+"\nVariable indefinida:\n"+indefinida+"\nVariable NULL:\n"+
-varNula+"\nArreglo con lista de frutas:\n"+frutas+"\nFuncion suma:\n"+"El resultado de la suma 10+5 es: " + suma(10, 5));
+varNula+"\nArreglo con lista de frutas:\n"+
+frutas+"\nFuncion suma:\n"+"El resultado de la suma 10+5 es: " + suma(10, 5) +
+"\nObjeto: \n" + JSON.stringify(objeto));
