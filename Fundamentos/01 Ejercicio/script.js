016752011f63fb1d6898142fda206fc947fb6dcb
kamasutras <ignaciolizana@liceovvh.cl>
2024-03-15 12:12:24 -0300

    ok

--- Fundamentos/01 Ejercicio/script.js
+++ Fundamentos/01 Ejercicio/script.js
@@ -1,30 +1,36 @@
 // ejerciocio 01 - Creacion de variables
 //una variable global de tipo var para mostrar el alcance global
-// Variable global con var 
 var global = "Global";
+let local = "Local";
+const constante = "Constante";
 // Variable local con let
-let total = "Local";
+
 let myDecimal = "15.6";
 let numEntero = "7";
-let boolean = true;
+let string="cadena";
+let booleanTrue = true;
+let booleanFalse = false;
+
 let indefinida;
 let varNula = null;
 // varialble con Const 
-const constante = "Constante";
+
 const frutas =  [[1, "melones"],[2, "peras"],[3, "sandias"],[4, "manzanas"]];
 /* const biografia = {
     nombre = "ignacio", nacionalidad = "chilena", profesion = "Estudiante del VVH"
     console.log("Mi nombre es " + nombre + " soy de nacionalidad " + nacionalidad  + " y soy " +  profesion)
 }; */
-console.log("Variables locales: " + global + " Variables Locales: " + " y " + "Variables constantes")
-console.log("mi decimal es, " + myDecimal);
-console.log("mi numero entero es, " + numEntero);
-console.log("mi booleano es: " + boolean);
+
 function suma(num1, num2){
     let resultado;
     resultado = num1 + num2;
     return resultado; 
 }
-console.log("la suma de 10+5 es = "+ suma(10, 5));
+//console.log("la suma de 10+5 es = "+ suma(10, 5));
 
 
+console.log("Variable const constante:\n"+constante+"Variable var global:\n"+global+"\nVariable (let) local:\n"+local+"\nVariable numero Decimal:\n"+
+myDecimal+"\nVariable numero entero:\n"+numEntero+"\nVariable String (Cadena):\n"
++string+"\nBoolean true (verdadero):\n"+booleanTrue+"\nBoolean false (falso):\n"+
+booleanFalse+"\nVariable indefinida:\n"+indefinida+"\nVariable NULL:\n"+
+varNula+"\nArreglo con lista de frutas:\n"+frutas+"\nFuncion suma:\n"+"El resultado de la suma 10+5 es: " + suma(10, 5));
