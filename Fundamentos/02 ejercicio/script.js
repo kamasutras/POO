9d0cac52fd8338e85703f6216679b86cafc48c99
kamasutras <ignaciolizana@liceovvh.cl>
2024-03-18 11:18:24 -0300

    ok

--- Fundamentos/02 ejercicio/script.js
+++ Fundamentos/02 ejercicio/script.js
@@ -1,52 +1,49 @@
 console.log("conectado correctamente");
 // Ejercicio 1: Declaración de variables
-// Instrucciones: Declara dos variables numéricas llamadas 'actual' y 'nac'.
+// Instrucciones: Declara dos variables numéricas llamadas 'ahora' y 'fecha_nac'.
 // Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
 
 function declararVariables() {
-    
-let actual = 2023;
-let nac = 2004;
-let resultado = actual - nac;
-alert("Mi edad es: " + resultado);
-// Tu código aquí
+    const ahora = 2024;
+    const fecha_nac = 2004;
+    const calculo = ahora - fecha_nac;
+    alert("tu edad actual es: " + calculo);
 }
 
 // Ejercicio 2: Asignación de valores
-// Instrucciones: Completa la función para asignar el valor de 'x' a 'y' y luego muestra el valor de 'y'.
+// Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
 function asignarValores() {
-let x = 15;
-let y = x;
-alert("El valor de Y es: " + y);
 
-// Tu código aquí
+let var1 = "hola mundo";
+let var2 = "hola universo";
+
+alert("el valor de var2 es: "+var2);
 }
 
 // Ejercicio 3: Operaciones matemáticas
 // Instrucciones: Completa la función para realizar las siguientes operaciones:
-// Suma 'a' y 'b', resta 'b' de 'a', multiplica 'a' por 'b', y divide 'a' entre 'b'.
+// Suma 'num1' , 'num2' y 'num3', resta 'num1' de 'num2', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
 // Muestra los resultados en un mensaje.
 function realizarOperaciones() {
-let b = 5;
-let a = 10;
-let suma = a + b;
-let resta = a - b;
-let multi = a * b;
-let divi = a / b;
+    let num1 = 20;
+    let num2 = 15;
+    let num3 = 20;
 
-alert("la suma es " + suma + "\n la resta es " + resta + "\n la multi es " + multi + "\n la division es " + divi );
+    let suma = num1+num2+num3;
+    let resta = num1-num2;
+    let multiplicar =num2*num3;
+    let division = multiplicar/120;
+alert("suma: "+suma + "\nresta: "+resta + "\nmultiplicacion: "+multiplicar + "\ndivision: "+division);
 
 // Tu código aquí
 }
 
 // Ejercicio 4: Manipulación de cadenas
-// Instrucciones: Completa la función para construir un mensaje utilizando las variables 'nombre' y 'apellido'.
-// Luego, muestra la longitud del mensaje y el primer carácter en el mensaje.
+// Instrucciones: Completa la función para construir un mensaje utilizando las variables 'Direccion' y 'n-casa'.
+// Luego, muestra la longitud del mensaje y el antepenultimo carácter en el mensaje.
 function manipularCadenas() {
-let nombre = "Juan";
-let apellido = "Pérez";
-let mensaje = "El se llama: " + nombre + " " + apellido;
-let longitud = mensaje.length;
-alert("Mensaje: " + mensaje +"\nLongitud: " + longitud +"\nPrimer Caracter: " + mensaje[0]);
-// Tu código aquí
+let direccion = avenida alpatacal;
+let numCasa =  9110;
+
+alert(direccion.length[0])
 }
