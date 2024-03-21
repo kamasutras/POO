d3bdb8b8ad2368a61d906b8db35d32af4467fcf4
kamasutras <ignaciolizana@liceovvh.cl>
2024-03-21 14:52:58 -0300

    ok

--- Fundamentos/04 Ejercicio/script.js
+++ Fundamentos/04 Ejercicio/script.js
@@ -6,6 +6,7 @@ function operandoMetodos() {
     let agregados = [];
     elementos.push(22, 24, 26, 28, 30);
     agregados.push(22, 24, 26, 28, 30);
+
     alert(`El método push() sirve para agregar uno o más elementos al final de un arreglo:\n ${elementos}\nValores agregados: ${agregados}`);
 
     // Metodo unshift()
@@ -13,13 +14,15 @@ function operandoMetodos() {
     let inicioAgregados = [];
     elementos.unshift(-8, -6, -4, -2, 0);
     inicioAgregados.push(-8, -6, -4, -2, 0);
-    alert(`El método unshift() sirve para agregar uno o más elementos al inicio de un arreglo:\n${elementos}\nEl valor agregado es: ${inicioAgregados}`);
+
+    alert(`El método unshift() sirve para agregar uno o más elementos al inicio de un arreglo:\n${elementos}\nValores agregado: ${inicioAgregados}`);
 
     //  Metodo shift()
     let primerValor = [];
     elementos = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
     elementos.shift();
     primerValor.push(elementos.shift());
+
     alert(`El metodo shift() sirbe para eliminar el primer elemento de un arreglo:\n ${elementos}\nel valor eliminmado es: ${primerValor}`);
 
     // Metodo pop()
@@ -35,12 +38,7 @@ function operandoMetodos() {
 
 
 function modificarMensaje() {
-    let mensajeInicial = ["Hola", "a", "todos", "los", "estudiantes", "de", "programación", "de", "4to", "C"]
+    let mensajeInicial = ["Hola", "a", "todos", "los", "estudiantes", "de", "programación", "de", "4to", "C"];
 
-    mensajeInicial.shift(); // Eliminar "Hola"
-    mensajeInicial.unshift("Somos", "nosotros");
-    mensajeInicial.push("mejores", "del mundo");
-
-
-    alert(` ${mensajeInicial}`);
+  
 }
