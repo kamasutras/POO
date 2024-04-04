3ba594412813fdaf2640c2c1e4caba4eeb1310ea
kamasutras <ignaciolizana@liceovvh.cl>
2024-04-04 12:26:57 -0300

    ok

--- Fundamentos/05 Condicionales/Ejemplos/script.js
+++ Fundamentos/05 Condicionales/Ejemplos/script.js
@@ -2,9 +2,9 @@
 function identificarMayorEdad() {
     let edad = prompt("Porfavor introdusca su edad: ");
 
-    if(edad >= 18){
-        alert("Eres mauyor de edad!")
-    } else{
-        alert("Lo sentimos no eres mayor de edad");
+    if (edad >= 18) {
+        alert(`Eres mauyor de edad - tiene: ${edad}`)
+    } else {
+        alert(`Eres menor de edad - tiene ${edad}`);
     }
 }
