9f7df935b07bb28e2f24c5b2ab25ad0b2ea24ace
kamasutras <ignaciolizana@liceovvh.cl>
2024-04-17 15:03:56 -0400

    ok

--- Fundamentos/06 Ejercicio/script.js
+++ Fundamentos/06 Ejercicio/script.js
@@ -22,14 +22,27 @@ function noNegative(arr) {
         let numero = parseInt(prompt("Ingrese los valores: "));
         array.push(numero);
     };
-    document.getElementById("elemento").innerHTML = `Los valores sin modificar: <h2>${array.join(", ")}</h2><br>`;
+    document.getElementById("elemento").innerHTML = `Los valores sin modificar: <h2>${array.join(", ")}</h2>`;
 
     for (let i = 0; i < l; i++) {
         if (array[i] < 0) {
             array[i] = 0;
         }
     };
+
+
+    // suma de los valores modificados
+    let suma = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        suma += array[i];
+    }
+
+    //const suma = array.reduce((acumulador, elemento) => acumulador + elemento, 0);
+
     document.getElementById("elemento").innerHTML += `Los valores negativos han sido modificados: <h2>${array.join(", ")}</h2>`;
+    document.getElementById("result").innerHTML += `La suma de los valores  modificados: <h2>${suma}</h2>`;
+
 };
 
 /* Recordatorio: para no alterar los array en este caso 
