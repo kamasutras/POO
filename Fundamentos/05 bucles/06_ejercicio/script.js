bfc73ad53526db94cdbb475a5c10706c03088d16
kamasutras <ignaciolizana@liceovvh.cl>
2024-04-17 10:01:08 -0400

    ok

--- Fundamentos/05 bucles/06_ejercicio/script.js
+++ Fundamentos/05 bucles/06_ejercicio/script.js
@@ -2,12 +2,12 @@ function calificar() {
     let suma = 0;
 
     for (let i = 0; i < 5; i++) {
-        let calificacion = parseFloat(prompt("Ingrese la calificación " + (i + 1) + ":"));
+        let calificacion = parseFloat(prompt(`Ingrese la calificación  ${i + 1} :`));
         suma += calificacion;
     }
 
     let promedio = suma / 5;
 
-    document.getElementById("resultado").innerHTML = "El promedio de las calificaciones es: " + promedio;
+    document.getElementById("resultado").innerHTML = `El promedio de las calificaciones es: ${promedio}`;
 
 }
