3056946f1c2704f86511cfd735d20bcd23c79b19
kamasutras <ignaciolizana@liceovvh.cl>
2024-04-25 09:10:33 -0400

    title rename inde -> index

---
+++ Fundamentos/07 Ejercicio/1/script.js
@@ -0,0 +1,13 @@
+function tablaDeN() {
+    let N = parseInt(document.getElementById("N").value);
+    let resultado = `La tabla de multiplicacion de N `;
+
+
+    for (let i = 1; i <= 10; i++) {
+        resultado += `${N} x ${i} = ${i * N}<br>`;
+    }
+
+
+    document.getElementById("resultado").innerHTML = resultado;
+
+}
