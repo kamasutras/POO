21994f2e0cb4e88eda83bfb679a61c4c67f34c6b
kamasutras <ignaciolizana@liceovvh.cl>
2024-04-29 14:20:31 -0400

    ok

--- Fundamentos/07 Ejercicio/2/script.js
+++ Fundamentos/07 Ejercicio/2/script.js
@@ -1,10 +1,10 @@
 function invertirSTR() {
     let strText = document.getElementById("strText").value;
-    let reversedStr = "";
+    let reverseStr = "";
 
     for (let i = strText.length - 1; i >= 0; i--) {
-        reversedStr += strText[i];
-        resultado = `<h2>La palabra invertida se escribe:</h2> <br>${reversedStr}`
+        reverseStr += strText[i];
+        resultado = `<h2>La palabra invertida se escribe:</h2> <br>${reverseStr}`
     }
 
     document.getElementById("resultado").innerHTML = resultado;
