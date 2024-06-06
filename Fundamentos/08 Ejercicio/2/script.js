69ca9e4c41af71bab78ab31061a9baa82b5f2b6e
kamasutras <ignaciolizana@liceovvh.cl>
2024-06-06 14:58:46 -0400

    ok

--- Fundamentos/08 Ejercicio/2/script.js
+++ Fundamentos/08 Ejercicio/2/script.js
@@ -2,7 +2,7 @@ function cambiarFondo(elemento) {
     //  console.log("elemento clickeado", elemento);
     if (elemento.textContent == 'Off') {
         elemento.style.backgroundColor = 'green'; // Cambia el texto del botón
-       elemento.style.color = 'white'; // Cambia el color de fondo del botón
+        elemento.style.color = 'white'; // Cambia el color de fondo del botón
         elemento.textContent = 'On'
     }
     else {
@@ -15,7 +15,7 @@ function cambiarFondo2(elemento) {
     //  console.log("elemento clickeado", elemento);
     if (elemento.textContent == 'Off') {
         elemento.style.backgroundColor = 'green'; // Cambia el texto del botón
-       elemento.style.color = 'white'; // Cambia el color de fondo del botón
+        elemento.style.color = 'white'; // Cambia el color de fondo del botón
         elemento.textContent = 'On'
     }
     else {
@@ -28,11 +28,60 @@ function cambiarFondo3(elemento) {
     //  console.log("elemento clickeado", elemento);
     if (elemento.textContent == 'Off') {
         elemento.style.backgroundColor = 'green'; // Cambia el texto del botón
-       elemento.style.color = 'white'; // Cambia el color de fondo del botón
+        elemento.style.color = 'white'; // Cambia el color de fondo del botón
         elemento.textContent = 'On'
     }
     else {
         elemento.style.backgroundColor = 'Red'
         elemento.textContent = 'Off'
+    }
+}
+
+function deleteicono1(elemento) {
+    if (elemento) {
+        elemento.remove();
+        console.log("Elemento eliminado correctamente.");
+    } else {
+        console.log("No se encontró el elemento para eliminar.");
+    }
+}
+function deleteicono2(elemento) {
+    if (elemento) {
+        elemento.remove();
+        console.log("Elemento eliminado correctamente.");
+    } else {
+        console.log("No se encontró el elemento para eliminar.");
+    }
+}
+function deleteicono3(elemento) {
+    if (elemento) {
+        elemento.remove();
+        console.log("Elemento eliminado correctamente.");
+    } else {
+        console.log("No se encontró el elemento para eliminar.");
+    }
+}
+function deleteicono4(elemento) {
+    if (elemento) {
+        elemento.remove();
+        console.log("Elemento eliminado correctamente.");
+    } else {
+        console.log("No se encontró el elemento para eliminar.");
+    }
+}
+function deleteicono5(elemento) {
+    if (elemento) {
+        elemento.remove();
+        console.log("Elemento eliminado correctamente.");
+    } else {
+        console.log("No se encontró el elemento para eliminar.");
+    }
+}
+function deleteicono6(elemento) {
+    if (elemento) {
+        elemento.remove();
+        console.log("Elemento eliminado correctamente.");
+    } else {
+        console.log("No se encontró el elemento para eliminar.");
     }
 }
