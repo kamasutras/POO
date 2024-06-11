9c6467bde189070ba289bdd998d7bb9305ced50c
kamasutras <ignaciolizana@liceovvh.cl>
2024-06-11 08:21:33 -0400

    a

---
+++ Fundamentos/inacap/python/arrays/ejercicio_1.py
@@ -0,0 +1,22 @@
+# Manejo de listas
+
+cantidadNotas = int(input("Cuantas notas quieres agregar? -> "))
+notas = [0] * cantidadNotas
+
+
+for i in range(0, cantidadNotas, 1):
+    notas[i] = float(input(f"Ingresa la nota  {i+1}-> "))
+else:
+    print(f"Finalizacion de la lectura de notas...\n\n\n")
+
+    
+    acumNotas = 0
+    for i in range(len(notas)):
+        acumNotas = acumNotas + notas[i]
+        promedio = acumNotas / len(notas)
+        
+        
+        print(f"tienes {cantidadNotas} notas y son las siguientes: {notas}")
+        print(f"\n\n\nPromedio Final:  {promedio}")
+        
+        
--- Fundamentos/inacap/python/arrays/ejercicio_2.py
+++ Fundamentos/inacap/python/arrays/ejercicio_2.py
@@ -1,5 +1,7 @@
 maxima = 0
-minima = 100
+minima = 5
+tmpminima= []
+tmpmaxima= []
 tmpMedidas = []
 print('-' * 40)
 print("Detectar temperatura mínima y máxima :D")
@@ -8,11 +10,21 @@ dias = int(input("Cantidad de días para medir la temperatura -> "))
 for i in range(1, dias + 1):
     temperatura = int(input(f"Temperatura del día {i} -> "))
     tmpMedidas.append(temperatura)
+    
     if temperatura > maxima:
         maxima = temperatura
     if temperatura < minima:
         minima = temperatura
         
-print(f"temperaturas tomadas en los ultimos {i} dias: {tmpMedidas}")
+        
+    tmpmaxima.append(maxima)
+    tmpminima.append(minima)
+
+
+
+print(f"las temperaturas minimas son: {tmpminima}")
+print(f" las temperaturas maximas son: {tmpmaxima}")
+
+print(f"\n\ntemperaturas tomadas en los ultimos {i} dias: {tmpMedidas}")
 print(f"Día con más calor: {maxima}")
 print(f"Día con menos calor: {minima}")
