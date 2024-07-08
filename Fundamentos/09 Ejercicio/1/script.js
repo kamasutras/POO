bd7029abcc43dfe6c11b03c438cfbece6aec4f99
kamasutras <ignaciolizana@liceovvh.cl>
2024-07-08 11:15:28 -0400

    lunes 8 de julio

---
+++ Fundamentos/09 Ejercicio/1/script.js
@@ -0,0 +1,5 @@
+function addName(){
+    let usr = document.querySelector("#nombres").value;
+    document.querySelector("#resultado").innerHTML += "<li>"+ usr+"</li>"
+
+}
---
+++ Fundamentos/09 Ejercicio/2/script.js
@@ -0,0 +1,7 @@
+function contador(elemento){
+    let contador = parseInt(elemento.getAttribute( 'data-contador') || '0');
+    contador++;
+    elemento.setAttribute( 'data-contador', contador);
+    elemento.textContent = 'clicks ' +  contador;
+
+}
