bb0e2bb1f1f298295cece222666e100237f0d900
kamasutras <ignaciolizana@liceovvh.cl>
2024-07-18 11:54:00 -0400

    ok

--- Fundamentos/09 Ejercicio/1/script.js
+++ Fundamentos/09 Ejercicio/1/script.js
@@ -1,5 +1,4 @@
 function addName(){
     let usr = document.querySelector("#nombres").value;
     document.querySelector("#resultado").innerHTML += "<li>"+ usr+"</li>"
-
 }
---
+++ Fundamentos/10 Ejercicio/script.js
@@ -0,0 +1,47 @@
+
+let count = 0;
+
+function countCards(card) {
+    switch(card) {
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+            count += 1;
+            break;
+        case '7':
+        case '8':
+        case '9':
+            break;
+        case '10':
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            count -= 1;
+            break;
+        default:
+            alert("Agrega un valor valido (Asegurese que J, Q, K sean mayusculas)");
+            break;
+    }
+
+    if (count > 0) {
+        return count + " Apostar";
+    } else {
+        return count + " Mantener";
+    }
+}
+
+function cc() {
+    count = 0; 
+    const input = document.getElementById("cardInput").value;
+    const cards = input.split(',').map(card => card.trim());
+    let result;
+
+    cards.forEach(card => {
+        result = countCards(card);
+    });
+
+    document.getElementById("resultado").innerHTML = "<h2>" + result + "</h2>";
+}
